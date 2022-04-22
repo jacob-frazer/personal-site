33714a29dd2b2dbe0cdb34c6081923bf9f62492b
Jacob Frazer <jakefrazer1997@googlemail.com>
2022-04-22 10:31:41 +0100

    Implemented navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from './Components/Home';
+import NavBar from './Components/NavBar';
+
+const navigation = {
+  brand: { name: "CobTech", to: "/" },
+  links: [
+    { name: "Products", to: "/products" },
+    { name: "Services", to: "/services" },
+    { name: "Blogs", to: "/blogs" },
+    { name: "Sign In", to: "/signin" },
+  ]
 }
 
-export default App;
+export default class App extends Component {
+  public render() {
+    // Descructured object for cleaner code :-)
+    const { brand, links } = navigation;
+
+    return (
+      <div className="App">
+        <NavBar brand={brand} links={links} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />}/>
+
+            {/* yet to be implemented */}
+            <Route path="/products" element={<div>PRODUCTS</div>}/>
+            <Route path="/services" element={<div>SERVICES</div>}/>
+            <Route path="/blogs" element={<div>BLOGS</div>}/>
+            <Route path="/signin" element={<div>SIGNIN</div>}/>
+          </Routes>
+        </BrowserRouter>
+        
+        <p>
+          The start of something great perhaps?
+        </p>
+      </div>
+    );
+  }
+};
---
+++ src/Components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component{
+    render(){
+      return(
+        <div id="homepage">
+          <h1>This is Cob Technologies</h1>
+          <p>Home page text and content in here</p>
+        </div>
+      )
+    }
+  }
+
+export default HomePage;
