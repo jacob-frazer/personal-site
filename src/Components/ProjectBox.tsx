852dbb9ed256ce1f393399fc15823329889679a5
Jacob Frazer <jakefrazer1997@googlemail.com>
2022-05-01 12:57:59 +0100

    Styling projects

--- src/Components/ProjectBox.tsx
+++ src/Components/ProjectBox.tsx
@@ -1,10 +1,74 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 import colours from '../utils/colours';
 
-const Background = styled.div`
+// css animations defined first
+const appearRotate = keyframes`
+  0% {
+    transform: scale(0.94) rotate(75deg);
+    opacity: 0.6;
+  }
+  100% {
+    transform: scale(1) rotate(0deg);
+    opacity: 1;
+  }
+`;
+
+const selectRotate = keyframes`
+  0% {
+    transform: rotateY(0deg);
+  }
+  100% {
+    transform: rotateY(360deg);
+  }
+`;
+
+// styled components
+const CentreIconDiv = styled.div`
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    `;
+
+const IconCircle = styled.div`
     background-color: rgb(${colours.light});
+    padding: 40px;
+    height: 75%;
+    aspect-ratio: 1/1;
+    border-radius: 50%;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    animation-name: ${appearRotate};
+    animation-duration: 1s;
+    `;
+
+const Image = styled.img`
+    height: 100%;
+    `;
+
+const TextBox = styled.div`
+    color: white;
+    font-size: 24pt;
+    font-weight: bold;
+    padding:10px;
+    font-family: Lucida Console, sans-serif;
+    `;
+
+const Background = styled.div`
+    background-color: rgb(${colours.mid});
     padding: 10px;
+    border-radius: 5%;
+    box-shadow: 7px 7px #888888;
+    &:hover {
+        background: rgb(${colours.light});
+        transform: scale(1.02);       
+    }
+    &:hover ${IconCircle} {
+        animation-name: ${selectRotate};
+        animation-duration: 0.5s;
+    }
     `;
 
 const ProjectBox = (props: {
@@ -13,8 +77,12 @@ const ProjectBox = (props: {
     const { proj } = props;
     return (
         <Background>
-            <div>{proj.name}: {proj.description}. Current project status: {proj.status} </div>
-            <img src={"/images/project_icons/" + proj.icon} alt={proj.name}></img>
+            <TextBox>{proj.name}: {proj.description}</TextBox>
+            <CentreIconDiv>
+                <IconCircle>
+                    <Image src={"/images/project_icons/" + proj.icon} alt={proj.name}></Image>
+                </IconCircle>
+            </CentreIconDiv>
         </Background>
         )
     };
